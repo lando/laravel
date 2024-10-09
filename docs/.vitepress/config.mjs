050589a7e1e6b2f4440400502bb34e10214a8375
Alec Reynolds <alec+git@lando.dev>
2024-10-09 16:27:25 -0700

    Update docs build to create multi-version builds. (#74)
    * Update docs build to create multi-version builds.
    
    * Build all doc versions from 1.0.0.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
     ['link', {rel: 'icon', href: '/laravel/favicon.svg', type: 'image/svg+xml'}],
   ],
   themeConfig: {
+    multiVersionBuild: {
+      satisfies: '>=1.0.0',
+    },
     sidebar: sidebar(),
   },
 });
